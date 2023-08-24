db1f9b7f6b95d80bb7252fe1c4f0294b5bd28a52
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-08-24 14:56:40 +0000

    Make apodization overlap compensation optional

--- vbeam/beamformers/__init__.py
+++ vbeam/beamformers/__init__.py
@@ -2,7 +2,6 @@ from typing import Sequence, Set, Union
 
 import vbeam.beamformers.building_blocks as building_blocks
 from vbeam.beamformers.building_blocks import (
-    compensate_for_apodization_overlap,
     do_nothing,
     sum_over_dimensions,
     unflatten_points,
@@ -17,16 +16,21 @@ from vbeam.util.transformations import *
 def get_das_beamformer(
     setup: SignalForPointSetup,
     *,
+    keep_dimensions: Union[Sequence[str], Set[str]] = (),
+    compensate_for_apodization_overlap: bool = True,
     log_compress: bool = True,
     scan_convert: bool = True,
-    keep_dimensions: Union[Sequence[str], Set[str]] = (),
 ):
     return compose(
         signal_for_point,
         vectorize_over_datacube(setup),
         sum_over_dimensions(setup, keep=keep_dimensions),
         unflatten_points(setup),
-        compensate_for_apodization_overlap(setup),
+        (
+            building_blocks.compensate_for_apodization_overlap(setup)
+            if compensate_for_apodization_overlap
+            else do_nothing
+        ),
         Apply(normalized_decibels) if log_compress else do_nothing,
         building_blocks.scan_convert(setup) if scan_convert else do_nothing,
     ).build(setup.spec)
