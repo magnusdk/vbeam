f3de5decdd58d9e268a681691ec67d604f6cd5cd
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-09-29 14:33:57 +0000

    Add combiner function to combine_apodizations
    By default, apodizations are combined by taking their minimum

--- vbeam/apodization/combine.py
+++ vbeam/apodization/combine.py
@@ -1,24 +1,30 @@
 from functools import reduce
 from operator import mul
-from typing import Iterable
+from typing import Callable, Iterable, Optional
 
 from vbeam.core import Apodization
+from vbeam.fastmath import numpy as np
 from vbeam.fastmath.traceable import traceable_dataclass
 
 
-@traceable_dataclass(("apodizations",))
+@traceable_dataclass(("apodizations",), ("combiner",))
 class CombinedApodization(Apodization):
     apodizations: Iterable[Apodization]
+    combiner: Optional[Callable[[np.ndarray], float]] = None
 
     def __call__(self, *args, **kwargs) -> float:
         """Multiply the result of calling the Apodization objects."""
-        values = [apodization(*args, **kwargs) for apodization in self.apodizations]
-        return reduce(mul, values, 1.0)
+        values = np.array([apod(*args, **kwargs) for apod in self.apodizations])
+        if self.combiner is not None:
+            return self.combiner(values)
+        return np.min(values)
 
 
 def combine_apodizations(
     *apodizations: Iterable[Apodization],
+    combiner: Optional[Callable[[np.ndarray], float]] = None,
 ) -> CombinedApodization:
-    """Return a new Apodization object that combines all the given apodizations
-    by multiplication."""
-    return CombinedApodization(apodizations)
+    """Return a new Apodization object that combines all the given apodizations.
+    
+    By default, apodizations are combined by taking the minimum of all their results."""
+    return CombinedApodization(apodizations, combiner)
