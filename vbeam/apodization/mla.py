6589ba3b5e3a59f5da8bfb1277e7a159cb2fd10a
AndersVra <anders.e.vralstad@ntnu.no>
2023-10-26 16:15:01 +0200

    set use_parent to aux

--- vbeam/apodization/mla.py
+++ vbeam/apodization/mla.py
@@ -6,7 +6,7 @@ from vbeam.util.geometry.v2 import Line
 from .window import Window
 
 
-@traceable_dataclass(("window", "beam_width", "use_parent"))
+@traceable_dataclass(data_fields=("window", "beam_width"), aux_fields=("use_parent",))
 class MLAApodization(Apodization):
     """Perform multiple line acquisition (MLA) in cartesian space.
 
@@ -27,7 +27,10 @@ class MLAApodization(Apodization):
         wave_data: WaveData,
     ) -> float:
         # Geometry assumes 2D points
-        sender_position = np.where(self.use_parent, sender.parent_element.position,sender.position)[np.array([0, 2])]
+        if self.use_parent:
+            sender_position = sender.parent_element.position[np.array([0, 2])]
+        else:
+            sender_position = sender.position[np.array([0, 2])]
         point_position = point_position[np.array([0, 2])]
         source = wave_data.source[np.array([0, 2])]
 
