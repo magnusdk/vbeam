0a43f1e6d9741875fe34df0573e2a434aa1bab3f
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2024-11-20 15:51:34 -0800

    Clean-up tests

--- tests/util/test_coordinate_systems.py
+++ tests/util/test_coordinate_systems.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from numpy import allclose, random
+from numpy import testing, random
 from vbeam.fastmath import Backend
 from vbeam.util.coordinate_systems import as_cartesian, as_polar
 
@@ -15,7 +15,11 @@ def test_inverse_conversions(np: Backend, jit_able: Callable[[Callable], Callabl
     # Test cases: origin, unit vector, and random points
     test_points = np.array(
         [
-            [0.0, 0.0, 0.0],  # origin
+            [
+                1e-6,
+                1e-6,
+                0.0,
+            ],  # near origin, except that theta/phi are not defined for x=y=z=0
             [1.0, 0.0, 0.0],  # unit vector
             [2.0, np.pi / 4, 1.0],
             [3.0, np.pi / 2, -1.0],
@@ -23,5 +27,9 @@ def test_inverse_conversions(np: Backend, jit_able: Callable[[Callable], Callabl
         ]
     )
 
-    assert allclose(pol2cart2pol(test_points), test_points)
-    assert allclose(cart2pol2cart(test_points), test_points)
+    testing.assert_allclose(
+        pol2cart2pol(test_points), test_points, rtol=1e-4, atol=1e-6
+    )
+    testing.assert_allclose(
+        cart2pol2cart(test_points), test_points, rtol=1e-4, atol=1e-6
+    )
