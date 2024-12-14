99dcbd7ff830cacfbcdce51064fefe12fbc11d57
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2024-12-13 17:12:12 -0800

    Don't np.prod over entire array, just along dimension of combined apodizations

--- vbeam/apodization/combine.py
+++ vbeam/apodization/combine.py
@@ -8,21 +8,24 @@ from vbeam.fastmath.traceable import traceable_dataclass
 @traceable_dataclass(("apodizations",), ("combiner",))
 class CombinedApodization(Apodization):
     apodizations: Iterable[Apodization]
-    combiner: Optional[Callable[[np.ndarray], float]] = None
+    combiner: Optional[Callable[list[np.ndarray], np.ndarray]] = None
 
-    def __call__(self, *args, **kwargs) -> float:
-        """Multiply the result of calling the Apodization objects."""
-        values = np.array([apod(*args, **kwargs) for apod in self.apodizations])
+    def __call__(self, *args, **kwargs) -> np.ndarray:
+        """Multiply the result of calling the Apodization objects.
+
+        The product is taken along a new axis to handle broadcasting correctly.
+        """
+        values = [apod(*args, **kwargs) for apod in self.apodizations]
         if self.combiner is not None:
             return self.combiner(values)
-        return np.prod(values)
+        return np.prod(np.array(values), axis=0)
 
 
 def combine_apodizations(
     *apodizations: Iterable[Apodization],
-    combiner: Optional[Callable[[np.ndarray], float]] = None,
+    combiner: Optional[Callable[[list[np.ndarray]], np.ndarray]] = None,
 ) -> CombinedApodization:
     """Return a new Apodization object that combines all the given apodizations.
-    
+
     By default, apodizations are combined by taking the product of all their results."""
     return CombinedApodization(apodizations, combiner)
