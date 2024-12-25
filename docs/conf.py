d81bcde0cddd60fbeaa54ddb3910dfc72bb99800
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2024-12-25 21:54:08 +0100

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ copyright = "2023, Magnus Dalen Kvalevåg"
 author = "Magnus Dalen Kvalevåg"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.8"
+release = "1.0.10"
 
 
 # -- General configuration ---------------------------------------------------
