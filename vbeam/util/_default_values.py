de705975a8dfcd03bb379aa3a6a42cf192d2c16e
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2025-01-15 09:30:07 +0100

    Add basic _default_values used for quick plotting

--- vbeam/util/_default_values.py
+++ vbeam/util/_default_values.py
@@ -1,42 +1,103 @@
-# TODO: Redo with spekk 2.0.0
-
-"""Module for creating reasonable default values for some arguments to 
+"""Module for creating reasonable default values for some arguments to
 :func:`vbeam.core.signal_for_point`. Intended for internal use only.
 
 Each function returns the default value and its spec."""
 
-from typing import Tuple
+from typing import Optional
 
 from spekk import ops
 
-from vbeam.core import ProbeGeometry, WaveData
-from vbeam.util.arrays import grid
+from vbeam.core import (
+    Apodization,
+    ChannelData,
+    GeometricallyFocusedWave,
+    Interpolator,
+    Probe,
+    ProbeElement,
+    ReflectedWaveDelayModel,
+    TransmittedWave,
+    TransmittedWaveDelayModel,
+)
+from vbeam.geometry import Direction, Orientation, Plane, Vector
+from vbeam.probe.aperture.rectangular_planar_aperture import RectangularPlanarAperture
 
 
-def _default_probe(spec: Spec) -> Tuple[ProbeGeometry, Spec]:
-    return ProbeGeometry(ops.zeros(3), ops.zeros(3), 10), spec.at["probe"].set([])
-
-
-def _default_receiver(spec: Spec) -> Tuple[Array, Spec]:
-    return ops.zeros(3), spec.at["receiver"].set([])
-
-
-def _default_sender(spec: Spec) -> Tuple[Array, Spec]:
-    return ops.zeros(3), spec.at["sender"].set([])
-
-
-def _default_point_position(spec: Spec) -> Tuple[Array, Spec]:
-    nx, nz = 200, 200
-    point_position = grid(
-        ops.linspace(-20e-3, 20e-3, nx),
-        ops.array([0.0]),
-        ops.linspace(0, 40e-3, nz),
-        shape=[nx, nz, 3],
+def points(n_x: int = 200, n_z: int = 200) -> ops.array:
+    points = ops.stack(
+        ops.meshgrid(
+            ops.linspace(-40e-3, 40e-3, n_x, dim="xs"),
+            ops.array([0.0], ["ys"]),
+            ops.linspace(-10e-3, 70e-3, n_z, dim="zs"),
+        ),
+        axis="xyz",
     )
-    return point_position, spec.at["point_position"].set(["x", "z"])
+    points = ops.squeeze(points, "ys")
+    points = ops.permute_dims(points, ("zs", "xs", "xyz"))
+    return points
 
 
-def _default_wave_data(spec: Spec) -> Tuple[WaveData, Spec]:
-    return WaveData(ops.array([5e-3, 0, 20e-3]), 0.0, 0.0, 0.0), spec.at[
-        "wave_data"
-    ].set([])
+def transmitting_probe(n_elements: int = 64) -> Probe:
+    element_positions = ops.stack(
+        [
+            ops.linspace(-50e-3, 50e-3, n_elements, dim="elements"),
+            ops.zeros(n_elements, dims=["elements"]),
+            ops.zeros(n_elements, dims=["elements"]),
+        ],
+        axis="xyz",
+    )
+    origin = ops.array([0, 0, 0], ["xyz"])
+    return Probe(
+        origin,
+        ProbeElement(element_positions, Orientation(0, 0, 0), 0.5e-3, 0.5e-3),
+        RectangularPlanarAperture(Plane(origin, Orientation(0, 0, 0)), 20e-3, 10e-3),
+    )
+
+
+def receiving_probe(n_elements: int = 64) -> Probe:
+    return transmitting_probe(n_elements)
+
+
+def transmitted_wave() -> TransmittedWave:
+    return GeometricallyFocusedWave(
+        ops.array([0, 0, 0], ["xyz"]),
+        Vector(50e-3, Direction(ops.pi / 8, 0)),
+    )
+
+
+def channel_data(
+    n_tx: int = 32,
+    n_elements: int = 64,
+    n_time_samples: int = 128,
+) -> ChannelData:
+    import numpy as np
+
+    signal = np.random.normal(0, 1, (n_tx, n_elements, n_time_samples))
+    return ChannelData(ops.array(signal, ["tx", "rx", "time"]), 0, 1e6)
+
+
+def interpolator() -> Interpolator:
+    raise NotImplementedError()  # TODO
+
+
+def modulation_frequency() -> Optional[float]:
+    return None
+
+
+def transmitted_wave_delay_model() -> TransmittedWaveDelayModel:
+    from vbeam.delay_models import SphericalDelayModel
+
+    return SphericalDelayModel()
+
+
+def reflected_wave_delay_model() -> ReflectedWaveDelayModel:
+    return ReflectedWaveDelayModel()
+
+
+def speed_of_sound() -> float:
+    return 1540.0
+
+
+def apodization() -> Apodization:
+    from vbeam.apodization import NoApodization
+
+    return NoApodization()
