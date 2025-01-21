fa18422a5b2d3a808f0bbfdbf85c7ab566c40888
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2025-01-21 11:23:27 +0100

    Add expanding aperture

--- vbeam/apodization/expanding_aperture.py
+++ vbeam/apodization/expanding_aperture.py
@@ -1,11 +1,15 @@
+# NOTE: Work-in-progress :)
+# TODO: Support curved probes. How does that work? 🤔
+
 from spekk import ops
 
 from vbeam.apodization.window import Window
 from vbeam.core import Apodization, GeometricallyFocusedWave, Probe
 
-# TODO
+
 class ExpandingAperture(Apodization):
     window: Window
+    f_number: float
 
     def __call__(
         self,
@@ -14,15 +18,49 @@ class ExpandingAperture(Apodization):
         point: ops.array,
         transmitted_wave: GeometricallyFocusedWave,
     ) -> float:
-        depths = ops.vecdot(
+        # TODO: Something smells bad with this line of code. What does it mean to the
+        # effective aperture from the probe for expanding aperture?
+        aperture = receiving_probe.get_effective_aperture(
+            receiving_probe.active_elements.normal
+        )
+
+        depth = ops.vecdot(
             point - receiving_probe.active_elements.position,
             receiving_probe.active_elements.normal.normalized_vector,
             axis="xyz",
         )
-        expanding_aperture = receiving_probe.active_aperture.set_origin(
-            receiving_probe.active_elements.position
-        )
-        expanding_aperture = expanding_aperture.set_size(1, 1)
-        expanding_aperture = expanding_aperture.scale(depths * 0.2)
 
-        return expanding_aperture.project_and_apply_window(point, self.window)
+        width = ops.abs(depth / self.f_number)
+        height = ops.abs(depth / self.f_number)
+
+        # Ensure width and height does not exceed the full aperture or is smaller than
+        # two elements (NOTE: the number "two" chosen a bit arbitrarily).
+        width = ops.clip(
+            width,
+            receiving_probe.active_elements.width * 2,
+            aperture.width,
+        )
+        height = ops.clip(
+            height,
+            receiving_probe.active_elements.height * 2,
+            aperture.height,
+        )
+
+        # TODO: This "bounding" logic should be part of the aperture, because not all
+        # apertures are rectangular. They may be circular.
+        # Convert to local plane 2D coordinates because that simplifies things a lot :)
+        x, y = aperture.plane.to_plane_coordinates(point)
+
+        # Clamp x and y so that the aperture doesn't fall outside of the full aperture.
+        x = ops.clip(x, (-aperture.width + width) / 2, (aperture.width - width) / 2)
+        y = ops.clip(y, (-aperture.height + height) / 2, (aperture.height - height) / 2)
+
+        # Convert back to glbal 3D coordinates.
+        point = aperture.plane.from_plane_coordinates(x, y)
+
+        # Transform the aperture into an expanding aperture originating from the
+        # receiving elements.
+        aperture = aperture.set_origin(receiving_probe.active_elements.position)
+        aperture = aperture.set_size(width=width, height=height)
+        # Project the point onto the aperture and apply window.
+        return aperture.project_and_apply_window(point, self.window)
