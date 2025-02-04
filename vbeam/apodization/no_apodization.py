80f45959c0c639935a30a285d6919563bf7da14e
tollef <tollef.jahren@gehealthcare.com>
2025-02-04 11:57:55 +0100

    Update NoApodization to return array

--- vbeam/apodization/no_apodization.py
+++ vbeam/apodization/no_apodization.py
@@ -1,8 +1,8 @@
 from vbeam.core import Apodization
-
+from spekk import ops
 
 class NoApodization(Apodization):
     """Always return 1.0 (no spatial weighting)."""
 
     def __call__(self, *_, **__) -> float:
-        return 1.0
+        return ops.array(1.0)
