a1569e0503ff2f20abf5390af0844a6cd1cd56ec
tollef <tollef.jahren@gehealthcare.com>
2025-02-04 11:59:52 +0100

    Update coherence_factor

--- vbeam/postprocess/coherence.py
+++ vbeam/postprocess/coherence.py
@@ -1,9 +1,25 @@
 from spekk import ops
 
 
-def coherence_factor(beamformed_data: ops.array, axis: int):
-    coherent = ops.abs(ops.sum(beamformed_data, axis=axis)) ** 2
-    incoherent = ops.sum(ops.abs(beamformed_data) ** 2, axis=axis)
+def coherence_factor(
+    beamformed_data: ops.array,
+    active_element_weights: ops.array,
+    *,
+    ord=2,
+    axis: int,
+):
+    
+    coherent   = ops.abs(ops.sum(beamformed_data, axis=axis)) ** ord
+    incoherent = ops.sum(ops.abs(beamformed_data) ** ord, axis=axis)
+
+    weight = active_element_weights
+    if axis in weight.dims:
+        weight = ops.sum(weight>0.0, axis=axis) ** (ord - 1)
+
     # Don't divide by 0!
     incoherent = ops.where(incoherent == 0, 1, incoherent)
-    return coherent / incoherent
+    weight = ops.where(weight == 0, 1, weight)
+    
+    return coherent / incoherent / weight
+    
+
