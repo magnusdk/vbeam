e780d610cec262ef5b26acca91b41e6acfb1d579
tollef <tollef.jahren@gehealthcare.com>
2025-03-04 11:30:58 +0100

    Update in debug script for plotting linear and irregular sampled coordinates

--- tests/interpolation/irregular_sampled_coordinates_test.py
+++ tests/interpolation/irregular_sampled_coordinates_test.py
@@ -14,18 +14,21 @@ def plot_linear_coordinates(index, n_samples):
 
     linear_coordinates = LinearCoordinates(ops.min(x), ops.max(x), x.size)
     # x_in = ops.linspace(0,9, 37, dim='depth')
-    x_in = ops.linspace(-2, 9, 45, dim='depth')
+    # x_in = ops.linspace(-2, 9, 45, dim='depth')
+    x_in = ops.linspace(-2, 11, 53, dim='xs')
     
     indices_info = linear_coordinates.get_nearest_indices(x_in, n_samples)
 
     nearest_indices_positions = indices_info.indices_positions[index]
-
+    nearest_indices_index = indices_info.indices[index]
+    
     plt.figure()
     plt.plot(x.data, x.data, '.', color='k', label='new samples')
     plt.plot(x_in.data, x_in.data, 'o', label='new samples',  mfc='none')
 
     for ii in range(n_samples):
         plt.plot(x_in[index], float(nearest_indices_positions[ii].data), '.', color='g')
+    plt.title(f"indices_positions={nearest_indices_positions.data} | indices_index={nearest_indices_index.data}, \n {nearest_indices_positions.data-x_in[index:index+1]}")       
     plt.show()
 
 
@@ -38,6 +41,7 @@ def plot_irregular_sampled_coordinates(index, n_samples):
     indices_info = irregular_sampled_coordinate.get_nearest_indices(x_in, n_samples)
     
     nearest_indices_positions = indices_info.indices_positions[index]
+    nearest_indices_index = indices_info.indices[index]
     
     plt.figure()
     plt.plot(x.data, x.data, '.', color='k', label='new samples')
@@ -45,16 +49,26 @@ def plot_irregular_sampled_coordinates(index, n_samples):
 
     for ii in range(n_samples):
         plt.plot(x_in[index], float(nearest_indices_positions[ii].data), '.', color='g')
-    plt.title(f"{nearest_indices_positions.data}, \n {nearest_indices_positions.data-x_in[index:index+1]}")
+    plt.title(f"indices_positions={nearest_indices_positions.data} | indices_index={nearest_indices_index.data}, \n {nearest_indices_positions.data-x_in[index:index+1]}")
     plt.show()
 
 if __name__== '__main__':
     
-    n_samples = 3
-    index = 30
+    # n_samples = 2
+    # index = 44
+    # index = 20
+    # index = 46
 
-    # plot_linear_coordinates(index, n_samples)
-    plot_irregular_sampled_coordinates(index, n_samples)
+
+    # plot_irregular_sampled_coordinates(index, n_samples)
+
+    n_samples = 2
+    # index = 44
+    # index = 8
+    index = 45
+    
+
+    plot_linear_coordinates(index, n_samples)
 
     a = 1
     
