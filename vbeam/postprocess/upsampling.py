ca224995d00f5a62515e5d8c150c00d8294cfa3a
tollef <tollef.jahren@gehealthcare.com>
2025-03-13 08:58:54 +0100

    Fix upsample_grid

--- vbeam/postprocess/upsampling.py
+++ vbeam/postprocess/upsampling.py
@@ -8,19 +8,12 @@ def upsample_grid(data: ops.array, n: int, axis: Dim):
 
     You should always upsample a Nyquist-sampled grid before envelope detection.
     """
-    # Create the n indices to sample the data at.
-    sample_indices = ops.arange(
-        n // 2,
-        n * data.dim_sizes[axis] - n // 2,
-        dim=axis,
-    )
-    sample_indices /= n
-    # Move the points slightly off the grid to get better noise statistics. For
-    # example, when n=2, the sampled indices are offset by 0.25
-    sample_indices -= (n - 1) / (2 * n)
+    assert n == 2
+    last_index = data.dim_sizes[axis] - 1
+    dim_size = data.dim_sizes[axis]
+    sample_indices = ops.linspace(0.25, last_index - 0.25, last_index * 2, dim=axis)
 
     # Upsample the data by interpolation at the sample indices
-    last_index = data.dim_sizes[axis] - 1
-    data_coordinates = {axis: LinearCoordinates(0, last_index, last_index)}
+    data_coordinates = {axis: LinearCoordinates(0, last_index, dim_size)}
     interpolator = LinearNDInterpolator(data_coordinates, data, fill_value=None)
     return interpolator({axis: sample_indices})
