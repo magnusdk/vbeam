6986033271ee3e62c9032ba836de36b5c96a17d9
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2025-03-20 13:50:27 +0100

    Fix expanding aperture (still WIP)

--- vbeam/apodization/expanding_aperture.py
+++ vbeam/apodization/expanding_aperture.py
@@ -22,40 +22,29 @@ class ExpandingAperture(Apodization):
         # effective aperture from the probe for expanding aperture?
         aperture = receiving_probe.get_effective_aperture(
             receiving_probe.active_elements.normal
-        ).set_origin(receiving_probe.active_elements.position)
-
-        depth = ops.vecdot(
-            point - receiving_probe.active_elements.position,
-            receiving_probe.active_elements.normal,
-            axis="xyz",
         )
 
-        width = ops.abs(depth / self.f_number)
-        height = ops.abs(depth / self.f_number)
+        depth = aperture.plane.signed_distance(point)
 
         # Ensure width and height does not exceed the full aperture or is smaller than
-        # two elements (NOTE: the number "two" chosen a bit arbitrarily).
-        width = ops.clip(
-            width,
-            receiving_probe.active_elements.width * 2,
-            aperture.width,
-        )
-        height = ops.clip(
-            height,
-            receiving_probe.active_elements.height * 2,
-            aperture.height,
-        )
+        # four elements (NOTE: the number "four" chosen a bit arbitrarily).
+        min_width = receiving_probe.active_elements.width * 4
+        min_height = receiving_probe.active_elements.height * 4
+        # Include two additional elements when finding the max width. Otherwise, the
+        # edge elements will be weighted to 0, because Tukey window becomes zero at
+        # either side.
+        max_width = aperture.width + receiving_probe.active_elements.width * 2
+        max_height = aperture.height + receiving_probe.active_elements.height * 2
+        width = ops.clip(depth / self.f_number, min_width, max_width)
+        height = ops.clip(depth / self.f_number, min_height, max_height)
 
-        # TODO: This "bounding" logic should be part of the aperture, because not all
-        # apertures are rectangular. They may be circular.
-        # Convert to local plane 2D coordinates because that simplifies things a lot :)
         x, y = aperture.plane.to_plane_coordinates(point)
+        x = ops.clip(x, (-max_width + width) / 2, (max_width - width) / 2)
+        y = ops.clip(y, (-max_width + height) / 2, (max_height - height) / 2)
 
-        # Clamp x and y so that the aperture doesn't fall outside of the full aperture.
-        x = ops.clip(x, (-aperture.width + width) / 2, (aperture.width - width) / 2)
-        y = ops.clip(y, (-aperture.height + height) / 2, (aperture.height - height) / 2)
-
-        # Transform the aperture into an expanding aperture originating from the
-        # receiving elements, apply window and return.
+        elements_x, elements_y = aperture.plane.to_plane_coordinates(
+            receiving_probe.active_elements.position
+        )
+        elements_x, elements_y = elements_x - x, elements_y - y
         aperture = aperture.set_size(width=width, height=height)
-        return aperture.apply_window(x, y, self.window)
+        return aperture.apply_window(elements_x, elements_y, self.window)
