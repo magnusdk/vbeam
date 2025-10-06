3743e2a2926aaddceb70014c22ef595e4771f89e
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2025-10-06 10:53:57 +0000

    Add constant f number apodization

--- vbeam/apodization/__init__.py
+++ vbeam/apodization/__init__.py
@@ -1,14 +1,13 @@
 import vbeam.apodization.window as window
+from vbeam.apodization.constant_f_number import ConstantFNumberApodization
 from vbeam.apodization.constant_width_apodization import ConstantWidthApodization
 from vbeam.apodization.expanding_aperture import ExpandingAperture
 from vbeam.apodization.no_apodization import NoApodization
+from vbeam.apodization.plane_wave import PlaneWaveTransmitApodization
+from vbeam.apodization.rtb import RTBApodization
 from vbeam.apodization.synthetic_transmit_aperture import (
     SyntheticTransmitApertureApodizationFNumber,
 )
-from vbeam.apodization.plane_wave import (
-    PlaneWaveTransmitApodization,
-)
-from vbeam.apodization.rtb import RTBApodization
 from vbeam.apodization.tx_rx_apodization import TxRxApodization
 from vbeam.apodization.window import (
     Bartlett,
@@ -27,6 +26,7 @@ from vbeam.core.apodization import Apodization
 
 __all__ = [
     "window",
+    "ConstantFNumberApodization",
     "ConstantWidthApodization",
     "ExpandingAperture",
     "NoApodization",
