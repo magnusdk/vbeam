c508f1d8b32eb922e3fd1cc47ec1209c43d41f49
tollef <tollef.jahren@gehealthcare.com>
2025-10-27 11:46:45 +0100

    fix in SyntheticTransmitApertureApodizationFNumber

--- vbeam/apodization/synthetic_transmit_aperture.py
+++ vbeam/apodization/synthetic_transmit_aperture.py
@@ -1,10 +1,51 @@
+from turtle import position
 from spekk import ops
 
-from vbeam.apodization.window import Window
+from vbeam.apodization.window import Window, NoWindow, Rectangular
 from vbeam.core import Apodization, Probe
 
+
 class SyntheticTransmitApertureApodizationFNumber(Apodization):
-    f_number: float=0.5
+    f_number: float = 0.5
+    minimum_aperture_width_x: float = 3e-3
+    minimum_aperture_width_y: float = 3e-3
+    # window: Window = Rectangular
+
+    def get_oneway_apodization(
+        self,
+        probe: Probe,
+        point: ops.array,
+    ) -> float:
+
+        dx = ops.abs(point["xyz", 0] - probe.active_elements.position["xyz", 0])
+        dy = ops.abs(point["xyz", 1] - probe.active_elements.position["xyz", 1])
+        dz = ops.abs(point["xyz", 2] - probe.active_elements.position["xyz", 2])
+
+        # Get valid points based on F-number
+        valid_fn_x = dx * 2 * self.f_number <= dz
+        valid_fn_y = dy * 2 * self.f_number <= dz
+
+        # Enforce minimum aperture width
+        valid_minimum_aperture_x = dx < self.minimum_aperture_width_x / 2
+
+        # valid_minimum_aperture_x = ops.abs(dx) < self.minimum_aperture_width_x / 2
+        valid_minimum_aperture_y = dy < self.minimum_aperture_width_y / 2
+   
+        return ops.logical_or(valid_fn_x * valid_fn_y, valid_minimum_aperture_x*valid_minimum_aperture_y)
+
+    def get_tx_apodization(
+        self,
+        transmitting_probe: Probe,
+        point: ops.array,
+    ):
+        return self.get_oneway_apodization(transmitting_probe, point)
+
+    def get_rx_apodization(
+        self,
+        receiving_probe: Probe,
+        point: ops.array,
+    ):
+        return self.get_oneway_apodization(receiving_probe, point)
 
     def __call__(
         self,
@@ -15,24 +56,7 @@ class SyntheticTransmitApertureApodizationFNumber(Apodization):
     ) -> float:
         "Return the spatial weighting at the given `point`."
 
-        # 3D boxcar apodization with f-number criteria
-        x_hd = point["xyz", 0]
-        y_hd = point["xyz", 1]
-        z_hd = point["xyz", 2]
-        
-        tx_probe_x = transmitting_probe.active_elements.position["xyz", 0]
-        tx_probe_y = transmitting_probe.active_elements.position["xyz", 1]
-        rx_probe_x = receiving_probe.active_elements.position["xyz", 0]
-        rx_probe_y = receiving_probe.active_elements.position["xyz", 1]
-        
-        # X-axis apodization (lateral)
-        apod_rx_x = (ops.abs(ops.atan((x_hd-rx_probe_x) / z_hd)) < self.f_number)
-        apod_tx_x = (ops.abs(ops.atan((x_hd-tx_probe_x) / z_hd)) < self.f_number)
-        
-        # Y-axis apodization (elevational)
-        apod_rx_y = (ops.abs(ops.atan((y_hd-rx_probe_y) / z_hd)) < self.f_number)
-        apod_tx_y = (ops.abs(ops.atan((y_hd-tx_probe_y) / z_hd)) < self.f_number)
-        
-        # Combined boxcar apodization: all conditions must be met
-        return apod_rx_x * apod_tx_x * apod_rx_y * apod_tx_y
+        return self.get_rx_apodization(
+            receiving_probe, point
+        ) * self.get_tx_apodization(transmitting_probe, point)
 
